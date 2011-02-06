3ca87dcc29ac9e563e2d5847cb114e43712ef7f4
Ilmari VackliN <ilmari.vacklin@cs.helsinki.fi>
2011-02-06 22:01:04 +0200

    Add fact about side effects to counting-spider

--- test/laziness/test_core.clj
+++ test/laziness/test_core.clj
@@ -1,9 +1,16 @@
 (ns laziness.test-core
   (:use laziness.core
-        midje.sweet))
+        midje.sweet)
+  (:import [java.io StringWriter]))
 
 (facts "counting-spider"
        (let [base "http://cs.helsinki.fi/u/ivacklin/"
              url-for-n (fn [n] (str base "clojure-" n ".txt"))
-             urls (map url-for-n [0 1 2])]
-         (counting-spider urls) => [0 1 2]))
+             urls (map url-for-n [0 1 2])
+             output (StringWriter.)]
+         (binding [*out* output]
+           (counting-spider urls)) => [0 1 2]
+         (str output) => (apply str (map #(str "Fetching " % "\n") urls))))
+
+(facts "print-squares"
+       )
