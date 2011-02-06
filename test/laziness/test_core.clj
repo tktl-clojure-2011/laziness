a0928c89ca6a566c89d7d3e4cabaefb9ebd555d4
Ilmari VackliN <ilmari.vacklin@cs.helsinki.fi>
2011-02-06 22:09:57 +0200

    rename capture-output to capturing-output

--- test/laziness/test_core.clj
+++ test/laziness/test_core.clj
@@ -3,7 +3,7 @@
         midje.sweet)
   (:import [java.io StringWriter]))
 
-(defmacro capture-output [f]
+(defmacro capturing-output [f]
   `(let [output# (StringWriter.)
          ret# (binding [*out* output#]
                 ~f)]
@@ -13,7 +13,7 @@
        (let [base "http://cs.helsinki.fi/u/ivacklin/"
              url-for-n (fn [n] (str base "clojure-" n ".txt"))
              urls (map url-for-n [0 1 2])]
-         (capture-output
+         (capturing-output
            (counting-spider urls))
-             => [[0 1 2] 
+             => [[0 1 2]
                  (apply str (map #(str "Fetching " % "\n") urls))]))
