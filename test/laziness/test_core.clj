3130fba28c814d6a5bf17c2a38db00ff2386e874
Ilmari VackliN <ilmari.vacklin@cs.helsinki.fi>
2011-02-06 22:08:14 +0200

    Refactor output capturing

--- test/laziness/test_core.clj
+++ test/laziness/test_core.clj
@@ -3,14 +3,17 @@
         midje.sweet)
   (:import [java.io StringWriter]))
 
+(defmacro capture-output [f]
+  `(let [output# (StringWriter.)
+         ret# (binding [*out* output#]
+                ~f)]
+     [ret# (str output#)]))
+
 (facts "counting-spider"
        (let [base "http://cs.helsinki.fi/u/ivacklin/"
              url-for-n (fn [n] (str base "clojure-" n ".txt"))
-             urls (map url-for-n [0 1 2])
-             output (StringWriter.)]
-         (binding [*out* output]
-           (counting-spider urls)) => [0 1 2]
-         (str output) => (apply str (map #(str "Fetching " % "\n") urls))))
-
-(facts "print-squares"
-       )
+             urls (map url-for-n [0 1 2])]
+         (capture-output
+           (counting-spider urls))
+             => [[0 1 2] 
+                 (apply str (map #(str "Fetching " % "\n") urls))]))
